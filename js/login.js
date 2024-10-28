3d97efb539ecc8fdbb488cfb750c835a10fbeb1e
Bruno Carletto Adamczyk <150372999+BruDu1545@users.noreply.github.com>
2024-10-28 12:13:20 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -47,3 +47,23 @@ function registerUser() {
         alert("Please fill in all the fields.");
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    let label_menu = document.querySelector('label[for="menu-toggle"]');
+    let menu_bar = document.querySelector('input#menu-toggle');
+    let toggle_list = document.getElementById('toggle-unorded-list-header');
+
+    menu_bar.addEventListener('change', function() {
+        if (menu_bar.checked) {
+            toggle_list.style.maxHeight = "500px"; 
+            toggle_list.style.padding = '15px'
+            toggle_list.style.gap = '15px';
+            toggle_list.style.textAlign = 'center';
+            label_menu.textContent = 'Fechar Menu';
+        } else {
+            toggle_list.style.maxHeight = "0";
+            toggle_list.style.padding = '0px'
+            label_menu.innerHTML = '<i class="fa-solid fa-bars"></i> Menu';
+        }
+    });
+});
